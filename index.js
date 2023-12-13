c06783f7b3906d8b53cf8c95928d0861f388744a
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2023-12-13 13:49:44 -0500

    commit 2

--- index.js
+++ index.js
@@ -10,8 +10,18 @@
 
 
 //Create Server and run on browser
-const http= require('http');
-http.createServer((req,res)=>{
-res.write("Hello, this is sahil!");
-res.end();
+// const http= require('http');
+// http.createServer((req,res)=>{
+// res.write("Hello, this is sahil!");
+// res.end();
+// }).listen(4500);
+
+
+//Simple API create ...step 1.Make server, 2.Create api header & n
+
+const http=require('http');
+http.createServer((req,resp)=>{
+resp.writeHead(200,{'Content-Type':'application\json'});
+resp.write(JSON.stringify({'name':'sahil', 'age':'30'}));
+resp.end();
 }).listen(4500);
