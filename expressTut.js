e172dcfab5fdb8f4634f751e9e72fac2fbff2c6f
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2023-12-18 14:13:55 -0500

    express routing

--- expressTut.js
+++ expressTut.js
@@ -2,3 +2,27 @@
 // ROUTING
 // MIDDLEWARE
 // WEB PAGES (html) and CSS INTEGRATION
+
+
+// *************************************
+
+// ROUTING IN EXPRESS JS : 
+
+const express = require('express');
+const app =express();
+
+app.get('/',(req, res)=>{
+res.send("This is Homepage")
+});
+
+app.get('/about',(req, res)=>{
+res.send("This is about page")
+})
+
+app.get('/contact',(req, res)=>{
+res.send("This is contact page")
+});
+
+app.listen(3000, ()=>{
+console.log("express server running on port 3000")
+})
