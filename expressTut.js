223de264f9897dad848c0dc82f8b26ed9ac2b14c
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2023-12-23 12:51:14 -0500

    html pages

--- expressTut.js
+++ expressTut.js
@@ -8,44 +8,44 @@
 
 // ROUTING IN EXPRESS JS : 
 
-const express = require('express');
-const app =express();
+// const express = require('express');
+// const app =express();
 
-app.get('/',(req, res)=>{
-    // url : http://localhost:4000/?name=sahil&age=26
-res.send("This is Homepage : "+req.query.age)               // getting data from query parameters
-console.log(req.query.name)
-});
+// app.get('/',(req, res)=>{
+//     // url : http://localhost:4000/?name=sahil&age=26
+// res.send("This is Homepage : "+req.query.age)               // getting data from query parameters
+// console.log(req.query.name)
+// });
 
-app.get('/about',(req, res)=>{
-res.send(`
-         <h1>This is about page</h1>
-         <a href="/contact">Go to contact page</a>                       
-            `)     // HTML H1 added  through "server response "   and    added links as well
-})
+// app.get('/about',(req, res)=>{
+// res.send(`
+//          <h1>This is about page</h1>
+//          <a href="/contact">Go to contact page</a>                       
+//             `)     // HTML H1 added  through "server response "   and    added links as well
+// })
 
-app.get('/contact',(req, res)=>{
-res.send('This is contact page :  <input type="text" placeholder="Page No"/>    <button>Submit</button>  ')    // HTML text input added     , and HTML button also added through "server response"
-});
+// app.get('/contact',(req, res)=>{
+// res.send('This is contact page :  <input type="text" placeholder="Page No"/>    <button>Submit</button>  ')    // HTML text input added     , and HTML button also added through "server response"
+// });
 
-app.get('/help',(req, res)=>{
-res.send(                                  // JSON data rendered through "server response"
-    [
-        {
-            name:"sahil",
-            age:26
-        },
-        {
-            hobby:"boxing",
-            level:"expert"
-        },
-    ]
-);
-});
+// app.get('/help',(req, res)=>{
+// res.send(                                  // JSON data rendered through "server response"
+//     [
+//         {
+//             name:"sahil",
+//             age:26
+//         },
+//         {
+//             hobby:"boxing",
+//             level:"expert"
+//         },
+//     ]
+// );
+// });
 
-app.listen(4000, ()=>{
-console.log("express server running on port 4000")
-})
+// app.listen(4000, ()=>{
+// console.log("express server running on port 4000")
+// })
 
 
 
@@ -59,4 +59,19 @@ console.log("express server running on port 4000")
 // res properties : res.send(), res.json(), res.status(), res.setHeader(), res.redirect(), res.render()
 
 
-// ejs
+
+
+
+// ***********************************
+
+
+// MAKE HTML PAGES
+
+const express= require('express');
+const app= express();
+const path = express('path')
+const pathFile = path.join(__dirname,'public');
+
+app.get('/',(req,res)=>{
+    res.sendFile()
+})
