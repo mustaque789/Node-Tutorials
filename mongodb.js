330f5637fcb8349550ea030b68bbbcd9d0f10606
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2023-12-25 22:50:52 -0500

    mongodb connection

---
+++ mongodb.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+
+// Remove the space in the URL
+const url = 'mongodb://127.0.0.1:27017';
+
+const database = 'e-comm';
+const client = new MongoClient(url);
+
+async function getData() {
+  let result = await client.connect();
+  let db = result.db(database);
+  let collection = db.collection('products');
+  let response = await collection.find({}).toArray();
+  console.log(response);
+}
+
+getData();
