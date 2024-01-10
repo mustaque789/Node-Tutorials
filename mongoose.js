aeac47441dbb906018f2f4fcb509dad69da61e89
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-01-10 12:23:08 -0500

    mongoose get

--- mongoose.js
+++ mongoose.js
@@ -1,2 +1,31 @@
 // schema : define data types
 // model : connect mongoose's collection via schema
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+
+const productSchema = new mongoose.Schema(
+    {
+        name:String,
+        price:Number,
+        brand:String,
+        category:String
+    }
+);
+
+const main = async ()=>{
+    const Product = mongoose.model('products',productSchema);
+
+    let data = new Product(
+        {
+            name:"Redmi",
+            price:500,
+            brand:"Xiomi",
+            category:"mobile"
+        }
+    )
+
+    const result = await data.save();
+    console.log(result)
+}
