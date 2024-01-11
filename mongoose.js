6d27c6391f66652e258027bb85ed9ed35796974f
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-01-11 12:55:40 -0500

    schema

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,9 @@
 // schema : define data types
 // model : connect mongoose's collection via schema
 
+
+// schema : validates data type
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
